93c4f9e3a015c698969250b6f0c46de4af60b34b
bzelaznicki <bartosz@zelaznicki.com>
2025-10-15 14:40:14 +0200

    searching for titles

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from lib.movie_title_search import search_command 
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "search":
+            print(f"Searching for: {args.query}")
+            found_movies = search_command(args.query)
+            
+            for idx, item in enumerate(found_movies): 
+                print(f"{idx+1}: {item}")
+                
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
---
+++ cli/lib/movie_title_search.py
@@ -0,0 +1,16 @@
+from .search_utils import (load_movies, DEFAULT_SEARCH_LIMIT)
+
+def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT):
+
+    found_movies = []
+
+    movies = load_movies()
+
+    for movie in movies:
+        if query in movie['title']:
+
+            found_movies.append(movie['title'])
+            if len(found_movies) >= limit:
+                break
+
+    return found_movies
