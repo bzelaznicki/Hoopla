3bd68a6ed0a045be4093f549e1eb1939552e4864
bzelaznicki <bartosz@zelaznicki.com>
2025-10-16 16:19:43 +0200

    removing punctuation from string

--- cli/lib/movie_title_search.py
+++ cli/lib/movie_title_search.py
@@ -1,16 +1,27 @@
+import string
 from .search_utils import (load_movies, DEFAULT_SEARCH_LIMIT)
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT):
 
+    query = process_text(query)
     found_movies = []
 
     movies = load_movies()
 
     for movie in movies:
-        if query.lower() in movie['title'].lower():
+        movie_title = process_text(movie['title'])
+        if query in movie_title: 
 
             found_movies.append(movie['title'])
             if len(found_movies) >= limit:
                 break
 
     return found_movies
+
+
+def process_text(text: str) -> str:
+    text = text.lower()
+    punc = str.maketrans("", "", string.punctuation)
+
+    text = text.translate(punc)
+    return text
